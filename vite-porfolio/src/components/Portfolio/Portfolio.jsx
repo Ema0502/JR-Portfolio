74cd0f5640981dd21143622aaffda2f2eaafc183
Ema0502 <ema050296@gmail.com>
2023-09-27 16:40:22 -0300

    portfolio + correcciones

---
+++ vite-porfolio/src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,56 @@
+import style from "./Portfolio.module.css";
+import { Container, Row, Col } from "reactstrap";
+import imgDogo from "../../img/Captura de pantalla (54).png"
+import imgPokemon from "../../img/Captura de pantalla (56).png"
+import imgVehiBuy from "../../img/Captura de pantalla 2023-09-27 153019.png"
+
+const Portfolio = () => {
+
+  const portfolioData = [
+    {
+      image: imgVehiBuy,
+      title: "VehiBuy",
+      url: "#"
+    },
+    {
+      image: imgDogo,
+      title: "Dogs App",
+      url: "#https://github.com/Ema0502/PI-Dogs-main"
+    },
+    {
+      image: imgPokemon,
+      title: "Pokemons App",
+      url: "#"
+    }
+  ]
+
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="12" className={style.portfolio__top}>
+            <h2>Portfolio</h2>
+          </Col>
+
+          {
+            portfolioData.map((item, index) =>(
+              <Col lg="4" md="6" sm="6" key={index}>
+                <div className={style.portfolio__card}>
+                  <div className={style.portfolio__img}><img src={item.image} alt="" /></div>
+
+                  <div className={style.portfolio__content}>
+                      <h6>{item.title}</h6>
+                      <a href={item.url}>Ver en vivo</a>
+                    <h5>Landing page</h5>
+                  </div>
+                </div>
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
+    </section>
+  )
+}
+
+export default Portfolio;
