473692a4e14bfc6f08f6a2d43dd9f5eb4f76f913
Ema0502 <ema050296@gmail.com>
2023-10-12 13:01:55 -0300

    svg icons agregados

--- vite-porfolio/src/components/About/Skills.jsx
+++ vite-porfolio/src/components/About/Skills.jsx
@@ -1,4 +1,18 @@
 import style from "./Skills.module.css"
+import cSharpIcon from "../../svg/csharp-icon.svg";
+import css3Icon from "../../svg/css-3-icon.svg";
+import dotnetIcon from "../../svg/dotnet-icon.svg";
+import expressIcon from "../../svg/express-icon.svg";
+import html5Icon from "../../svg/html-5-icon.svg";
+import javaScriptIcon from "../../svg/javascript-icon.svg";
+import noseJsIcon from "../../svg/node-js-icon.svg";
+import postgreSqlIcon from "../../svg/postgresql-icon.svg";
+import reactJsIcon from "../../svg/react-icon.svg";
+import reduxIcon from "../../svg/redux-icon.svg";
+import sequelizeIcon from "../../svg/sequelize-icon.svg";
+import typeScriptIcon from "../../svg/typescript-icon.svg";
+import gitIcon from "../../svg/git-icon.svg";
+import gitHubIcon from "../../svg/github-icon.svg";
 
 const Skills = () => {
 
@@ -6,27 +20,44 @@ const Skills = () => {
     {
       title: "JavaScript",
       percentage: "100%",
-      grade: "Avanzado"
+      grade: "Avanzado",
+      icon: javaScriptIcon
     },
     {
       title: "React js",
       percentage: "100%",
-      grade: "Avanzado"
+      grade: "Avanzado",
+      icon: reactJsIcon
     },
     {
       title: "Redux",
       percentage: "100%",
-      grade: "Avanzado"
+      grade: "Avanzado",
+      icon: reduxIcon
     },
     {
       title: "HTML",
       percentage: "100%",
-      grade: "Avanzado"
+      grade: "Avanzado",
+      icon: html5Icon
     },
     {
       title: "CSS",
       percentage: "100%",
-      grade: "Intermedio"
+      grade: "Intermedio",
+      icon: css3Icon
+    },
+    {
+      title: "Git",
+      percentage: "100%",
+      grade: "Intermedio",
+      icon: gitIcon
+    },
+    {
+      title: "GitHub",
+      percentage: "100%",
+      grade: "Intermedio",
+      icon: gitHubIcon
     }
   ]
 
@@ -34,27 +65,44 @@ const Skills = () => {
     {
       title: "Node js",
       percentage: "100%",
-      grade: "Avanzado"
+      grade: "Avanzado",
+      icon: noseJsIcon
     },
     {
       title: "Express",
       percentage: "100%",
-      grade: "Avanzado"
-    },
-    {
-      title: "MySQL",
-      percentage: "100%",
-      grade: "Avanzado"
+      grade: "Avanzado",
+      icon: expressIcon
     },
     {
       title: "TypeScript",
       percentage: "100%",
-      grade: "Inicial"
+      grade: "Inicial",
+      icon: typeScriptIcon
     },
     {
-      title: "Nest js",
+      title: "PostgreSQL",
       percentage: "100%",
-      grade: "Inicial"
+      grade: "Avanzado",
+      icon: postgreSqlIcon
+    },
+    {
+      title: "Sequelize",
+      percentage: "100%",
+      grade: "Avanzado",
+      icon: sequelizeIcon
+    },
+    {
+      title: "C#",
+      percentage: "100%",
+      grade: "Inicial",
+      icon: cSharpIcon
+    },
+    {
+      title: ".NET",
+      percentage: "100%",
+      grade: "Inicial",
+      icon: dotnetIcon
     }
   ]
 
@@ -65,6 +113,7 @@ const Skills = () => {
           frontendSkills.map((item, index) => (
             <div className={style.skill__data} key={index}>
               <div className={style.skill__title}>
+                <img src={item.icon} alt="Programming Icon" />
                 <h6>{item.title}</h6>
                 <span>{item.grade}</span>
               </div>
@@ -82,6 +131,7 @@ const Skills = () => {
           backendSkills.map((item, index) => (
             <div className={style.skill__data} key={index}>
               <div className={style.skill__title}>
+                <img src={item.icon} alt="Programming Icon" />
                 <h6>{item.title}</h6>
                 <span>{item.grade}</span>
               </div>
