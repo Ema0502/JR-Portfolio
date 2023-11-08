7bb91bb921fc6733a4ae7189f604e337694fb3ab
Ema0502 <ema050296@gmail.com>
2023-11-08 15:57:47 -0300

    header responsive corregido

--- vite-porfolio/src/components/Header/Header.jsx
+++ vite-porfolio/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
+import { useEffect, useState } from "react";
 import { Container } from "reactstrap";
 import style from "./Header.module.css";
-import { useEffect, useState } from "react";
 import logo_JR from "../../img/Captura de pantalla 2023-08-12 230401.png";
 
 const Header = () => {
@@ -36,9 +36,35 @@ const Header = () => {
   ];
 
   const handleClickLogo = () => {
-    const bodyElement = document.querySelector('body');
-    bodyElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    const bodyElement = document.querySelector("body");
+    bodyElement.scrollIntoView({ behavior: "smooth", block: "start" });
   };
+
+  const handleMenuToggle = (event) => {
+    event.stopPropagation();
+    setIsMenuActive(!isMenuActive);
+  };
+
+  const handleDocumentClick = () => {
+    if (isMenuActive) {
+      setIsMenuActive(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsHeaderShrink(window.scrollY > 80);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, [isMenuActive]);
 
   const handleClick = (event) => {
     event.preventDefault();
@@ -53,28 +79,17 @@ const Header = () => {
     setIsMenuActive(false);
   };
 
-  const handleMenuToggle = () => {
-    setIsMenuActive(!isMenuActive);
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsHeaderShrink(window.scrollY > 80);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div className={`${style.header} ${isHeaderShrink ? style.header__shrink : ""}`}>
       <Container>
         <div className={style.navigation}>
           <div className={style.logo}>
-            <img className={style.logoJR} src={logo_JR} alt="Logo..." onClick={handleClickLogo} />
+            <img
+              className={style.logoJR}
+              src={logo_JR}
+              alt="Logo..."
+              onClick={handleClickLogo}
+            />
           </div>
 
           <div
@@ -92,7 +107,16 @@ const Header = () => {
           </div>
 
           <div className={style.nav__right}>
-            <button className={style.btn}><a aria-label="Chat en WhatsApp" href="https://wa.me/543876298577" target="_blank" rel="noopener noreferrer"><i className="ri-whatsapp-line"></i> Consultas</a></button>
+            <button className={style.btn}>
+              <a
+                aria-label="Chat en WhatsApp"
+                href="https://wa.me/543876298577"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="ri-whatsapp-line"></i> Consultas
+              </a>
+            </button>
             <span className={style.mobile__menu} onClick={handleMenuToggle}>
               <i className="ri-menu-5-line"></i>
             </span>
